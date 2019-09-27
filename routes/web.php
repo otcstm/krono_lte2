2845e9d3ebd432c102b9d845885de3a23bc72d61
afdzal3 <afdzal3@gmail.com>
2019-09-27 16:03:26 +0800

    State maintenance page.

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,23 @@
 */
 
 Route::redirect('/', '/login');
+
+//start state admin
+//Route::get( '/admin/states'         ,'StateController@create'   )->name('state.create');
+Route::post('/admin/states'         ,'StateController@store'    )->name('state.store');
+Route::get( '/admin/restState'      ,'StateController@list'     )->name('state.list');
+Route::post('/admin/state/destroy'  ,'StateController@destroy'  )->name('state.destroy');
+Route::get( '/admin/state/show'    ,'StateController@show'   )->name('state.show');
+Route::post( '/admin/state/update'    ,'StateController@update'   )->name('state.update');
+//end state admin
+
+
 Auth::routes(['register' => false]);
 
 // Route::get('/', 'MiscController@index')->name('misc.index');
 Route::group(['middleware' => ['auth']], function () {
-  Route::get('/home', 'MiscController@home')->name('misc.home');
+ Route::get('/home', 'MiscController@home')->name('misc.home');
+ 
+
+ 
 });
