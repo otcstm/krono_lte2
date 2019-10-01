5daebf7aa495c0781b4d1dbff78318afabe2add3
afdzal3 <afdzal3@gmail.com>
2019-10-01 12:05:40 +0800

    Update state to force login

--- app/Http/Controllers/Admin/StateController.php
+++ app/Http/Controllers/Admin/StateController.php
@@ -1,34 +1,34 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\State;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
-
-
+use App\Http\Controllers\Controller;
 
 class StateController extends Controller
 {
-
-    public function __construct(){
+    public function __construct()
+    {
         //abort_if(Gate::denies('admin_roles'), Response::HTTP_FORBIDDEN, '403 Forbidden');
     }
 
-    public function show(State $state)
+    public function show(Request $req)
     {
-
         $states = $this->list();
 
-        $alert = isset($alert) ? $alert : ''; 
-
-        return view('admin.states',['states' => $states,]);
+        $alert = Session('alert') ? Session('alert') : 'rest';
+        $ac = Session('ac') ? Session('ac') : 'info';
+        //$ac = isset($ac) ? $ac : 'info';
+        //dd(Session('alert'));
+        return view('admin.states', ['states' => $states,'alert'=>$alert, 'ac'=>$ac
+        ]);
     }
 
     public function update(Request $req)
     {
-
         $state_var = State::findOrFail($req->id);
         $state_var->state_descr = $req->state_descr;
         $state_var->save();
@@ -37,33 +37,30 @@ class StateController extends Controller
         $ac = 'info';
         $alert = 'updated ' . $req->state_code .':' .$req->state_descr;
         $states = $this->list();
-        return view('admin.states',['states' => $states, 'alert'=>$alert, 'ac'=>$ac]);
-        //return redirect()->route('state.show',);
+        //return view('admin.states',['states' => $states, 'alert'=>$alert, 'ac'=>$ac]);
+        return redirect()->route('state.show', [
+          'alert' => $alert,
+          'ac'=>$ac
+        ]);
     }
 
     public function store(Request $req)
     {
-
         $state_check = State::find($req->state_code);
         $alert = "string";
         $ac = 'info';
 
-        if(!$state_check){
-        $state_var = new State;
-        $state_var->id          = $req->state_code;
-        $state_var->state_descr = $req->state_descr;
-        $state_var->save();
-        $alert = 'created ' . $req->state_code .':' .$req->state_descr;
-        }
-        else{
+        if (!$state_check) {
+            $state_var = new State;
+            $state_var->id          = $req->state_code;
+            $state_var->state_descr = $req->state_descr;
+            $state_var->save();
+            $alert = 'created ' . $req->state_code .':' .$req->state_descr;
+        } else {
             $ac = 'danger';
             $alert = $req->state_code .' already existed and will not be added';
-
         }
-
-        $states = $this->list();
-        return view('admin.states',['states' => $states,'alert'=>$alert, 'ac'=>$ac]);
-
+        return redirect()->route('state.show')->with(['alert' => $alert, 'ac'=>$ac]);
     }
 
 
@@ -73,20 +70,15 @@ class StateController extends Controller
         $states = $this->list();
         $st = $req->state_id;
         State::destroy($st);
-
-
-
         $ac = 'info';
         $alert = $req->state_id .' has been destroyed';
-        $states = $this->list();
-        return view('admin.states',['states' => $states,'alert'=>$alert, 'ac'=>$ac]);
 
+        return redirect()->route('state.show')->with(['alert' => $alert, 'ac'=>$ac]);
     }
 
-    public static function list(){
+    public static function list()
+    {
         $states = State::all();
         return $states;
-
     }
-
 }
