506927fb0c9da2ce2467dfeae0f97a49c0ee4240
Mohamad Hizamuddin Bin Jamaluddin <TM31777@TM.MY>
2019-10-02 14:50:16 +0800

    new

---
+++ database/migrations/2019_10_01_095437_create_user_logs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('module_name');
+            $table->string('activity_type');
+            $table->string('session_id');
+            $table->string('ip_address');
+            $table->string('user_agent');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_logs');
+    }
+}
