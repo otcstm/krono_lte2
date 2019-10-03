9a7ac4b1ed9604274e2849ad9ef5f555ca65690f
Mohd Khafizi Bin Amat Noh <TM36886@TM.MY>
2019-10-03 16:41:26 +0800

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,29 +49,37 @@ class LoginController extends Controller
 
       $udata = LdapHelper::DoLogin($req->staff_no, $req->password);
 
-      if($udata['code'] == 200){
-        // session(['staffdata' => $logresp['user']]);
+      //=========== use this when coding at work ===========
+      // if($udata['code'] == 200){
+      //   // session(['staffdata' => $logresp['user']]);
 
-        $cuser = User::where('staff_no', $req->staff_no)->first();
-        if($cuser){
-        } else {
-          // temporary: use ldap data to create user
-          // $udata = LdapHelper::FetchUser($req->staff_no, 'cn');
-          $cuser = new User;
-          $cuser->staff_no = $udata['data']['STAFF_NO'];
-          $cuser->email = $udata['data']['EMAIL'];
-          $cuser->name = $udata['data']['NAME'];
-          // $cuser->persno = $udata['data']['PERSNO'];
-          $cuser->new_ic = $udata['data']['NIRC'];
-          $cuser->save();
+      //   $cuser = User::where('staff_no', $req->staff_no)->first();
+      //   if($cuser){
+      //   } else {
+      //     // temporary: use ldap data to create user
+      //     // $udata = LdapHelper::FetchUser($req->staff_no, 'cn');
+      //     $cuser = new User;
+      //     $cuser->staff_no = $udata['data']['STAFF_NO'];
+      //     $cuser->email = $udata['data']['EMAIL'];
+      //     $cuser->name = $udata['data']['NAME'];
+      //     // $cuser->persno = $udata['data']['PERSNO'];
+      //     $cuser->new_ic = $udata['data']['NIRC'];
+      //     $cuser->save();
 
-          // also give the super admin role lol
-          // $cuser->roles()->attach(1);
-        }
+      //     // also give the super admin role lol
+      //     // $cuser->roles()->attach(1);
+      //   }
 
-        Auth::loginUsingId($cuser->id, true);
-        return redirect()->intended(route('misc.home', [], false));
-      }
+      //   Auth::loginUsingId($cuser->id, true);
+      //   return redirect()->intended(route('misc.home', [], false));
+      // }
+      //=========== use this when coding at work ===========  
+
+      //=========== use this when coding at home ===========
+      $cuser = User::where('staff_no', $req->staff_no)->first();
+      Auth::loginUsingId($cuser->id, true);
+      return redirect()->intended(route('misc.home', [], false));
+      //=========== use this when coding at home ===========
 
       return redirect()->back()->with('message', $udata['msg']);
 
