0e0011120f377cbc685ae17de0b6c6a7209fa232
Nur Syazwani Binti Jusoh <TM35733@TM.MY>
2019-11-05 08:44:38 +0800

    clock in - Separate clock transaction by date (multiple day)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,10 +14,7 @@ class DatabaseSeeder extends Seeder
       ini_set('max_execution_time', 3000); // 300 seconds = 5 minutes
       set_time_limit(0);
         $this->call(AmerPermissionSeeder::class);
-<<<<<<< HEAD
-=======
 
->>>>>>> 292f9cc7dd7a1feaa2825a49c91a43e69196790f
         // $this->call(RolesTableSeeder::class);
         // $this->call(RoleUserTableSeeder::class);
         //$this->call(SapPersdataSeeder::class);
