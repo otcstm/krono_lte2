7547b921b10c4fc427b4c96da43680e784baaa74
Nur Syazwani Binti Jusoh <TM35733@TM.MY>
2019-11-19 11:52:29 +0800

    ot rates & setup codes table

---
+++ database/migrations/2019_11_18_085741_create_ot_rates_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOtRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ot_rates', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->string('company_id',10);
+          $table->string('region', 25);
+          $table->string('legacy_codes');
+          $table->string('day_type', 25);
+          $table->string('wagetype');
+          $table->string('descr')->nullable();
+          $table->string('claim_type');
+          $table->string('unit');
+          $table->decimal('rate',2,2);
+          $table->decimal('salary_cap',10,2)->nullable();
+          $table->date('start_date')->nullable();
+          $table->date('end_date')->nullable();
+          $table->integer('created_by');
+          $table->integer('last_edited_by')->nullable();
+          $table->integer('deleted_by')->nullable();
+          $table->softDeletes();
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ot_rates');
+    }
+}
