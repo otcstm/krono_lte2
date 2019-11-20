77d00c45fa4f39aeba6589ac5d3ff91acbb0b5cd
Nur Syazwani Binti Jusoh <TM35733@TM.MY>
2019-11-20 11:43:27 +0800

    commit

--- database/migrations/2019_11_20_031138_create_overtime_formulas_table.php
+++ database/migrations/2019_11_20_031138_create_overtime_formulas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOtRatesTable extends Migration
+class CreateOvertimeFormulasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,21 +13,18 @@ class CreateOtRatesTable extends Migration
      */
     public function up()
     {
-        Schema::create('ot_rates', function (Blueprint $table) {
+        Schema::create('overtime_formulas', function (Blueprint $table) {
           $table->bigIncrements('id');
-          $table->string('company_id',10);
+          $table->bigInteger('compregconfig_id');
           $table->string('region', 25);
-          $table->string('legacy_codes');
+          $table->string('legacy_codes')->nullable();
           $table->string('day_type', 25);
-          $table->string('wagetype');
+          $table->string('wagetype')->nullable();
           $table->string('descr')->nullable();
           $table->integer('min_hour');
           $table->integer('max_hour');
           $table->string('unit');
-          $table->integer('divbyhour');
-          $table->integer('divbyday');
-          $table->decimal('multiple_by',2,2);
-          $table->decimal('salary_cap',10,2)->nullable();
+          $table->decimal('rate',2,2);
           $table->date('start_date')->nullable();
           $table->date('end_date')->nullable();
           $table->integer('created_by');
@@ -45,6 +42,6 @@ class CreateOtRatesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ot_rates');
+        Schema::dropIfExists('overtime_formulas');
     }
 }
---
+++ database/migrations/2019_11_20_031631_create_comp_region_configs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompRegionConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comp_region_configs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('comp_region_id');
+            $table->decimal('salary_cap',10,2)->nullable();
+            $table->integer('hourperday');
+            $table->integer('daypermonth');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comp_region_configs');
+    }
+}
