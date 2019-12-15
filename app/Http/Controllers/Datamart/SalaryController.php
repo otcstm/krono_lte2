feb6ed69c502d216098a1da443f0a2b98a41bace
Mohd Afdzal Bin Kamal <afdzal3@gmail.com>
2019-12-15 22:19:19 +0800

    salary api

--- app/Http/Controllers/Datamart/SalaryController.php
+++ app/Http/Controllers/Datamart/SalaryController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class SalaryController extends Controller
 {
+  public static function insert(
+      $user_id,
+      $upd_sap,
+      $start_date,
+      $end_date,
+      $payscale_type,
+      $payscale_area,
+      $salary
+  ) {
+    $s = new Salary;
+
+}
 
 }
