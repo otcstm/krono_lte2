5eecc8c512a853537c0036f1134251fcb4533095
Nur Syazwani Binti Jusoh <TM35733@TM.MY>
2020-02-12 12:50:17 +0800

    ..

--- resources/views/report/otr.blade.php
+++ resources/views/report/otr.blade.php
@@ -1,7 +1,6 @@
 @extends('adminlte::page')
 @section('css')
-<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
-@stop
+
 @section('title', 'Report')
 @section('content')
 
