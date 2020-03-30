bcc735de9b7e37cbaa1bb2d583012351895754a0
Afdzal <afdzal3@gmail.com>
2020-03-30 17:07:03 +0800

    otindicator

---
+++ app/Api/Datamart/OtIndicatorController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\Api\Datamart;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\OtIndicator;
+
+class OtIndicatorController extends Controller
+{
+
+
+  public function insert(Request $req)
+  {
+
+    $exOtInd = OtIndicator::where('persno', $req->pers_no)
+    ->where('start_date',$req->start_date_ot_ind)->delete();
+
+    $oti = new OtIndicator;
+
+    $oti->user_id             = $req->pers_no;
+    $oti->upd_sap             = $req->change_on_ot_ind;
+    $oti->start_date          = $req->start_date_ot_ind;
+    $oti->end_date            = $req->end_date_ot_ind;
+
+    $oti->ot_hour_exception   = $req->ot_hour_exception;
+    $oti->ot_salary_exception = $req->ot_salary_exception;
+    $oti->allowance           = $req->allowance_amount;
+    $oti->allowance           = $req->allowance_amount;
+    $oti->upd_dm              = $req->last_upd_dt;
+    $oti->save();
+
+    return $oti->id;
+
+  }
+
+  public function returnMaxDate()
+  {
+      $upd_sap = OtIndicator::max('upd_sap');
+
+      return $upd_sap;
+
+  }
+
+
+
+
+}
---
+++ database/migrations/2020_03_30_081345_create_ot_indicators_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtIndicatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ot_indicators', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->dateTime('upd_sap')->nullable();
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('end_date')->nullable();
+            $table->string('ot_salary_exception', 1)->nullable();
+            $table->string('ot_hour_exception', 1)->nullable();
+            $table->decimal('allowance', 10, 3)->default(0.0);
+            $table->dateTime('upd_dm')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ot_indicators');
+    }
+}
