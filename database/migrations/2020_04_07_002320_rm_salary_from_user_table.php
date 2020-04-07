e0f8c94b6ae776abf683ed430e13849d8d0e8455
Afdzal Kamal <afdzal3@gmail.com>
2020-04-07 09:26:12 +0800

    rm salary from user/user_records

---
+++ database/migrations/2020_04_07_002320_rm_salary_from_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RmSalaryFromUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->dropColumn('ot_salary_exception');
+          $table->dropColumn('ot_hour_exception');
+          $table->dropColumn('salary'); 
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->string('ot_salary_exception', 1)->after('persarea')->nullable();
+          $table->string('ot_hour_exception', 1)->after('perssubarea')->nullable();
+          $table->decimal('salary',10,2)->default(0.00);
+
+        });
+    }
+}
---
+++ database/migrations/2020_04_07_012141_rm_salary_from_user_record_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RmSalaryFromUserRecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_records', function (Blueprint $table) {
+          $table->dropColumn('ot_salary_exception');
+          $table->dropColumn('ot_hour_exception');
+          $table->dropColumn('salary');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_records', function (Blueprint $table) {
+          $table->string('ot_salary_exception', 1)->after('persarea')->nullable();
+          $table->string('ot_hour_exception', 1)->after('perssubarea')->nullable();
+          $table->decimal('salary',10,2)->default(0.00);
+        });
+    }
+}
