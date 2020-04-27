82b01905c26c0011e54b148f2d047a4b001b8490
khvfizi <khafizi.amatnoh@gmail.con>
2020-04-28 06:20:40 +0800

    test

--- resources/views/email/ot/otquery.blade.php
+++ resources/views/email/ot/otquery.blade.php
@@ -1,4 +1,5 @@
 @component('mail::message')
+
 Dear **{{$toname}}**,
 
 Your overtime application **{{ $claim }}** has successfully been queried.<br>
@@ -11,4 +12,5 @@ Please click on [Overtime System](https://otcs-test.tm.com.my/) to access Overti
 This is a system generated email from [Overtime System](https://otcs-test.tm.com.my/).
 If you have any queries / complaints related to overtime, kindly channel them through [HC SSO Helpdesk](https://precise.tm.com.my/) (IDM Login & Password > Incident Catalog > HCSSO - Helpdesk).
 Please do not reply to this email.
+
 @endcomponent
