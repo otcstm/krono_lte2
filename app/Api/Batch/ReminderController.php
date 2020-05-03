f71ed0ec38dd62dcc472c741feb7cfefaa2e6adf
deazth@gmail.com <deazth@gmail.com>
2020-05-03 15:20:39 +0800

    api to trigger reminder

---
+++ app/Api/Batch/ReminderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\Api\Batch;
+
+use Illuminate\Routing\Controller as BaseController;
+use \Carbon\Carbon;
+
+class ReminderController extends BaseController
+{
+  public function process()
+  {
+
+
+    return 'Done';
+  }
+
+}
