d32bf52a18aa5e02d46f5e82752f083c38834924
khvfizi <khafizi.amatnoh@gmail.con>
2020-06-25 16:40:54 +0800

    eligibility n css fix

--- app/Http/Controllers/MiscController.php
+++ app/Http/Controllers/MiscController.php
@@ -384,7 +384,7 @@ class MiscController extends Controller
 
   public function eligiblePunch(Request $req){
     $staffr = URHelper::getUserRecordByDate($req->user()->id, date('Y-m-d'));
-    $elig = OvertimeEligibility::where('company_id', $staffr->company_id)->where('empgroup', $staffr->psgroup)->where('empsgroup', $staffr->empsgroup)->where('psgroup', $staffr->empgroup)->where('region', $staffr->region)->first();
+    $elig = OvertimeEligibility::where('company_id', $staffr->company_id)->where('empgroup', $staffr->psgroup)->where('empsgroup', $staffr->empsgroup)->where('psgroup', $staffr->empgroup)->where('region', $staffr->region)->where('start_date','<=', date('Y-m-d'))->where('end_date','>', date('Y-m-d'))->first();
     if($elig){
       return ['result'=> true];
     }else{
