b78678531f493536e912ac28d36cbb712c96984e
Afdzal <afdzal3@gmail.com>
2020-07-01 15:20:12 +0800

    login offline prod removal

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::redirect('/', '/login');
 Auth::routes(['register' => false]);
 //Temporary offline login (url /login/offline)
 //Route::view('/login/offline', 'loginoffline', []);
-Route::post('/login/offline', 'TempController@login')->name('login.offline');
+//Route::post('/login/offline', 'TempController@login')->name('login.offline');
 //User record controller
 Route::get('/ur/popbyid/{id}', 'URController@popById')->name('ur.popbyid');
 Route::get('/ur/show/{persno}', 'URController@show')->name('ur.show');
