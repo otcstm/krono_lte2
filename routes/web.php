7ecfdbd137fa7e3ea5a89fd2a4ae4adff7d8c5d5
Afdzal <afdzal3@gmail.com>
2020-07-15 09:37:00 +0800

    login dev

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ Route::redirect('/', '/login');
 Auth::routes(['register' => false]);
 //Temporary offline login (url /login/offline)
 
-//Route::view('/login/offline', 'loginoffline', []);
-//Route::post('/login/offline', 'TempController@login')->name('login.offline');
+Route::view('/login/offline', 'loginoffline', []);
+Route::post('/login/offline', 'TempController@login')->name('login.offline');
 
 //User record controller
 Route::get('/ur/popbyid/{id}', 'URController@popById')->name('ur.popbyid');
