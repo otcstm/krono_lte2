65d750ec9b1d9d3be08824005998d065e87c056b
Mohamad Hizamuddin Bin Jamaluddin <mohamad.hizamuddin@tm.com.my>
2020-10-05 17:59:42 +0800

    Fixes item no 54

--- app/Http/Controllers/OvertimeController.php
+++ app/Http/Controllers/OvertimeController.php
@@ -733,7 +733,23 @@ class OvertimeController extends Controller
             } else {
                 Session::put(['draft' => []]);
             }
-            Session::put(['claim' => $claim]);
+            Session::put(['claim' => $claim]);      
+
+            //item no 54
+            //20201005 fix unstable update overtimemonth table. 
+            //update every selection date
+            if($claim){
+                $upd_total_min_hour = Overtime::where('month_id',$claim->month_id)->sum('total_minute')/60;
+                $upd_total_hour = Overtime::where('month_id',$claim->month_id)->sum('total_hour');     
+                $upd_total_hour = $upd_total_hour+$upd_total_min_hour;         
+                $upd_total_min = Overtime::where('month_id',$claim->month_id)->sum('total_minute')%60;
+
+                $updatemonth = OvertimeMonth::find($claim->month_id);
+                $updatemonth->total_hour = $upd_total_hour;
+                $updatemonth->total_minute = $upd_total_min;
+                $updatemonth->save();
+            }
+
             return redirect(route('ot.form', [], false));
         } else {
             Session::put(['draft' => [], 'claim' => []]);
