5487657556f24c6ba2d917fd55a16004d76a8361
Afdzal <afdzal3@gmail.com>
2021-05-11 22:28:24 +0800

    visitors

---
+++ database/factories/PageCounterFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PageCounter;
+use Faker\Generator as Faker;
+
+$factory->define(PageCounter::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
